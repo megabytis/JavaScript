dacc1faa6647f9f48e1f2eeaba91576d6e19e958
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-06-28 21:09:29 +0530

    updating
    usage of forEach in sets and maps

--- 8. advArrayOperations/3. forEachLoop.js
+++ 8. advArrayOperations/3. forEachLoop.js
@@ -19,3 +19,39 @@ HOW forEach() works ?
 -> This callback runs once for each element in namesArr.
 
 */
+
+// Another IMPORTANT point
+namesArr.forEach(function (names, indexNums, namesArray) {
+  console.log(`${indexNums}: ${names}`);
+  //   console.log(namesArray);
+});
+// ✍️ NOTEDOWN :--
+// forEach's callback func;
+// 1st take 'value', then 'index' as param
+
+// ######## forEach in maps #########
+const school = new Map([
+  ["name", "DAV Public School"],
+  ["location", "Bhubaneswar"],
+  ["NumOfStudents", 5000],
+]);
+
+school.forEach(function (value, key, school) {
+  console.log(`${key}: ${value}`);
+  //   console.log(school);
+});
+// here also, like forEach in array , i.e.
+// forEach's callback function 1st take value, then key as param
+
+// ######## forEach in sets #########
+
+const fruits = new Set([
+  "apple",
+  "banana",
+  "grapes",
+  "custard apple",
+  "litchi",
+]);
+fruits.forEach((fruit) => {
+  console.log(fruit);
+});

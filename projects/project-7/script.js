5709837a64aef2c2369187bfa6d8274a056aa306
megabytis <166828632+megabytis@users.noreply.github.com>
2025-07-18 12:59:09 +0530

    update in project js

--- projects/project-7/script.js
+++ projects/project-7/script.js
@@ -3,6 +3,7 @@
 ///////////////////////////////////////
 // Modal window
 
+const header = document.querySelector(".header");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
@@ -31,3 +32,34 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// creating & inserting elements
+const messege = document.createElement("div");
+messege.classList.add("cookie-message");
+messege.innerHTML =
+  "Hey this website uses cookies for better improvement & functionality <button class='btn btn--close-cookie' > OK </button>";
+
+// header.prepend(messege); // it will plaece the messege div at the top of the page
+header.append(messege); // it will plaece the messege div at the end of the page
+// so after typing both, it won't create 2 copies of it 🤣, rather it will place the div at bottom, cuz, 'append' been called lastly :)
+
+// header.prepend(messege.cloneNode(true)); // it will create clones of the div :), i.e. top & bottom, on both side it'll now be availabe
+
+// Deleting elements
+document.querySelector(".btn--close-cookie").addEventListener("click", (e) => {
+  // messege.remove();
+  // ------or-----
+  messege.parentElement.removeChild(messege);
+});
+
+// styles
+messege.style.backgroundColor = "rgb(55,56,61";
+messege.style.width = "120%";
+
+// // logging any element's property's value
+// console.log(messege.style.height);
+// console.log(messege.style.backgroundColor);
+
+// more effective way to log element's property's value
+console.log(getComputedStyle(messege).color);
+console.log(getComputedStyle(messege).height);
